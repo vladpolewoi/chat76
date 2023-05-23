dd993eef26ab3b8790717cd292ce0008f555e193
vlad polevoi <vlad.polewoi1@gmail.com>
2023-05-23 22:28:23 +0300

    feat: :sparkles: authContext + users page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import ToasterContext from '@/app/context/ToasterContext'
-import './globals.css'
+import { AuthContext } from '@/app/context/AuthContext'
 import { Inter } from 'next/font/google'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +14,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   )
